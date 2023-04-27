47dd908a1fd6086de363fa6e3fea5f34f3983e84
Eren Eroğlu <108634315+ereneroglum@users.noreply.github.com>
2023-04-27 05:19:05 +0300

    Changed emacs config inline with emacs 29.

---
+++ emacs/.config/emacs/early-init.el
@@ -0,0 +1,54 @@
+;;; early-init.el --- Libera Emacs early-init file  -*- lexical-binding: t; -*-
+
+;; Copyright (c) 2022-2023  Eren Eroğlu <108634315+ereneroglum@users.noreply.github.com>
+;; URL: https://github.com/ereneroglum
+;; Version: 1.0.0
+;; Package-Requires ((emacs "29.1"))
+
+;; This file is NOT part of GNU Emacs.
+
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU Affero General Public License as published by
+;; the Free Software Foundation, version 3 of the License.
+;;
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU Affero General Public License for more details.
+;;
+;; You should have received a copy of the GNU Affero General Public License
+;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Code:
+
+;; Package Management
+(setq package-enable-at-startup nil)
+
+;; Configure GC
+(setq gc-cons-threshold (* 1024 1024 1024))
+
+;; Backups
+(unless (file-directory-p (concat user-emacs-directory "auto-save"))
+  (make-directory (concat user-emacs-directory "auto-save")))
+(setq auto-save-file-name-transforms `((".*" ,(concat user-emacs-directory "auto-save" "/") t))
+      backup-by-copying t
+      delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      backup-directory-alist `(("" . ,(concat user-emacs-directory "backup"))))
+
+;; Startup Screen
+(setq inhibit-splash-screen t
+      inhibit-startup-screen t)
+
+;; Dialog Boxes
+(setq use-dialog-box nil
+      use-file-dialog nil)
+
+;; UI
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(push '(font . "JetBrainsMono Nerd Font Mono 12") default-frame-alist)
+
+;;; early-init.el ends here
--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,33 +1,29 @@
-;; Package Management
-(require 'package)
-(package-initialize)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
-(setq package-selected-packages
-      '(vscode-dark-plus-theme company eglot helm magit which-key go-mode nix-mode rust-mode))
-(when (cl-find-if-not #'package-installed-p package-selected-packages)
-  (package-refresh-contents)
-  (mapc #'package-install package-selected-packages))
+;;; init.el --- Libera Emacs init file  -*- lexical-binding: t; -*-
 
-;; Backups
-(unless (file-directory-p (concat user-emacs-directory "auto-save"))
-  (make-directory (concat user-emacs-directory "auto-save")))
-(setq auto-save-file-name-transforms `((".*" ,(concat user-emacs-directory "auto-save" "/") t))
-      backup-by-copying t
-      delete-old-versions t
-      kept-new-versions 6
-      kept-old-versions 2
-      backup-directory-alist `(("" . ,(concat user-emacs-directory "backup"))))
+;; Copyright (c) 2022-2023  Eren Eroğlu <108634315+ereneroglum@users.noreply.github.com>
+;; URL: https://github.com/ereneroglum
+;; Version: 1.0.0
+;; Package-Requires ((emacs "29.1"))
+
+;; This file is NOT part of GNU Emacs.
+
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU Affero General Public License as published by
+;; the Free Software Foundation, version 3 of the License.
+;;
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU Affero General Public License for more details.
+;;
+;; You should have received a copy of the GNU Affero General Public License
+;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Code:
 
 ;; UI
 (global-display-line-numbers-mode 1)
-(global-hl-line-mode 1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(xterm-mouse-mode 1)
-(set-frame-font "JetBrainsMono Nerd Font Mono 12")
-(load-theme 'vscode-dark-plus t)
+(load-theme 'modus-vivendi)
 
 ;; Show number of matching phrases
 (setq isearch-lazy-count t)
@@ -41,23 +37,11 @@
 ;; Auto Close Brackets
 (electric-pair-mode 1)
 
-;; Helm
-(global-set-key (kbd "C-x C-f") #'helm-find-files)
-(global-set-key (kbd "M-x") #'helm-M-x)
-(helm-mode 1)
-(helm-autoresize-mode 1)
-
-;; Which Key Mode
-(which-key-mode 1)
-
-;; Company Mode Config
-(setq company-idle-delay 0.1
-      company-minimum-prefix-length 1)
-(global-company-mode 1)
-
 ;; Eglot Config
 (add-hook 'c++-mode-hook 'eglot-ensure)
 (add-hook 'c-mode-hook 'eglot-ensure)
-(add-hook 'go-mode-hook 'eglot-ensure)
-(add-hook 'python-mode-hook 'eglot-ensure)
-(add-hook 'rust-mode-hook 'eglot-ensure)
+
+;; Switch to completions buffer
+(setq completion-auto-select t)
+
+;;; init.el ends here
